ac5dddb452c0010507dcc387ef7f5860b126516d
inventivetalent <inventivetalent@inventivegames.de>
2016-06-22 19:40:38 +0200

    remove callSuper

--- src/main/java/org/spiget/data/resource/version/ListedResourceVersion.java
+++ src/main/java/org/spiget/data/resource/version/ListedResourceVersion.java
@@ -8,10 +8,8 @@ import lombok.ToString;
  * Represents a resource version fetched from the resources list
  */
 @Data
-@EqualsAndHashCode(callSuper = true,
-				   doNotUseGetters = true)
-@ToString(callSuper = true,
-		  doNotUseGetters = true)
+@EqualsAndHashCode(doNotUseGetters = true)
+@ToString(doNotUseGetters = true)
 public class ListedResourceVersion {
 
 	protected String name;
