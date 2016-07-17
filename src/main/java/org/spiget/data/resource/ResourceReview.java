8c27dcfe5f5c52e25cf838c084e41fbab330ba4a
inventivetalent <inventivetalent@inventivegames.de>
2016-07-17 17:22:25 +0200

    use ListedAuthor

--- src/main/java/org/spiget/data/resource/ResourceReview.java
+++ src/main/java/org/spiget/data/resource/ResourceReview.java
@@ -3,19 +3,19 @@ package org.spiget.data.resource;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.ToString;
-import org.spiget.data.author.Author;
+import org.spiget.data.author.ListedAuthor;
 
 @Data
 @EqualsAndHashCode(doNotUseGetters = true)
 @ToString(doNotUseGetters = true)
 public class ResourceReview {
 
-	protected Author author;// Not necessarily an author, but a reference to the member who posted this review
-	protected Rating rating;
-	protected String message;
-	protected String version;
-	protected long   date;
-	protected Author responseAuthor;
-	protected String responseMessage;
+	protected ListedAuthor author;// Not necessarily an author, but a reference to the member who posted this review
+	protected Rating       rating;
+	protected String       message;
+	protected String       version;
+	protected long         date;
+	protected ListedAuthor responseAuthor;
+	protected String       responseMessage;
 
 }
