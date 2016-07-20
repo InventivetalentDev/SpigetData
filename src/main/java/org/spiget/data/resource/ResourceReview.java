33d51234935f1c92ffa3ee1fdc5084d89831a256
inventivetalent <inventivetalent@inventivegames.de>
2016-07-20 19:01:42 +0200

    add id to reviews again

--- src/main/java/org/spiget/data/resource/ResourceReview.java
+++ src/main/java/org/spiget/data/resource/ResourceReview.java
@@ -3,12 +3,15 @@ package org.spiget.data.resource;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.ToString;
+import org.spiget.data.IdReference;
 import org.spiget.data.author.ListedAuthor;
 
 @Data
-@EqualsAndHashCode(doNotUseGetters = true)
-@ToString(doNotUseGetters = true)
-public class ResourceReview {
+@EqualsAndHashCode(doNotUseGetters = true,
+				   callSuper = true)
+@ToString(doNotUseGetters = true,
+		  callSuper = true)
+public class ResourceReview extends IdReference {
 
 	protected ListedAuthor author;// Not necessarily an author, but a reference to the member who posted this review
 	protected Rating       rating;
