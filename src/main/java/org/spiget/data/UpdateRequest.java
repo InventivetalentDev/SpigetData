bd66b472ac231ead046119ab6a4a6a72a82e6531
inventivetalent <mail@inventivetalent.org>
2018-02-13 16:44:29 +0100

    Create UpdateRequest

---
+++ src/main/java/org/spiget/data/UpdateRequest.java
@@ -0,0 +1,16 @@
+package org.spiget.data;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(doNotUseGetters = true)
+public class UpdateRequest {
+
+	protected int     requestedId;
+	protected long    requested;
+	protected boolean versions;
+	protected boolean updates;
+	protected boolean reviews;
+
+}
