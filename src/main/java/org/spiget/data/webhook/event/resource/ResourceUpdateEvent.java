ae70aae82d77d4c55e4991f6ddd001f732c82917
inventivetalent <mail@inventivetalent.org>
2020-02-06 19:43:50 +0100

    add updatedId field

--- src/main/java/org/spiget/data/webhook/event/resource/ResourceUpdateEvent.java
+++ src/main/java/org/spiget/data/webhook/event/resource/ResourceUpdateEvent.java
@@ -12,17 +12,28 @@ public class ResourceUpdateEvent extends WebhookEvent {
 
 	private final Resource resource;
 	private final String   newVersion;
+	private final int updateId;
 
+	@Deprecated
 	public ResourceUpdateEvent(Resource resource, String newVersion) {
 		super("resource-update");
 		this.resource = resource;
 		this.newVersion = newVersion;
+		this.updateId = -1;
+	}
+
+	public ResourceUpdateEvent(Resource resource, String newVersion, int updateId) {
+		super("resource-update");
+		this.resource = resource;
+		this.newVersion = newVersion;
+		this.updateId = updateId;
 	}
 
 	@Override
 	public JsonObject dataToJson() {
 		JsonObject jsonObject = GSON.toJsonTree(this.resource).getAsJsonObject();
 		jsonObject.addProperty("newVersion", newVersion);
+		jsonObject.addProperty("updateId", updateId);
 		return jsonObject;
 	}
 
